f38909ffb444917cbb339ad3e235d2b68e8cc78f
Timur Nurlygaianov <t.nurlygaianov@semrush.com>
2020-12-24 20:20:36 +0200

    Updated test

--- test_petfriends_api.py
+++ test_petfriends_api.py
@@ -1,6 +1,14 @@
 import requests
 
 
-def test():
-    res = requests.get('https://mail.ru')
+def test_get_api_key():
+    """ Get API Key and check the length of the key. """
+
+    url = 'https://petfriends1.herokuapp.com/api/key'
+    res = requests.get(url, headers={'email': 'api@mail.ru',
+                                     'password': 'test'})
+    data = res.json()
+
     assert res.status_code == 200
+    assert len(data['key']) == 20
+
